d4c891f5b074ce0c98e673ad0cd77874882bc302
Denis Semenyuk <semenyuk@MacBook-Air-Denis.local>
2026-06-08 15:09:37 +0500

    Закончил с запросом API

--- src/components/api.js
+++ src/components/api.js
@@ -4,13 +4,33 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 const api = `https://www.googleapis.com/books/v1/volumes?q=harry+potter&key=${API_KEY}`;
 
 function RequestBooks() {
+ const [books, setBooks] = useState([]);
+
   useEffect(() => {
     fetch(api)
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => {
+        console.log(data);
+        setBooks(data.items || [])})
   }, []);
 
-  return <h1>Books</h1>;
+  return (
+    <div>
+      <h1>Books</h1>
+      <ul>
+        {books.map(book => (
+          <li key={book.id}>
+            <h2>{book.volumeInfo.title}</h2>
+            <p>Автор: {book.volumeInfo.authors}</p>
+            {book.volumeInfo.imageLinks?.thumbnail && (
+              <img src={book.volumeInfo.imageLinks.thumbnail} alt="book title"/>
+            )}
+            <p>{book.volumeInfo.description}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 export default RequestBooks;
